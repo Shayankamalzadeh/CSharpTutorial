12e4f55289e1a280cc49d2a8978fa83ecd5c468e
Shayan Kamalzadeh <shayan.kamalzadeh@cadmatic.com>
2023-04-13 14:28:35 +0200

    first commit

---
+++ myFirstApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x= ConvertMeterToCM(3);
+
+
+SayHello();
+
+
+
+
+void SayHello()
+{
+ Console.WriteLine ("Hello, world");
+}
+
+int ConvertMeterToCM(int metter)
+{
+ 
+    return metter*100;
+}
+
+
+
+
